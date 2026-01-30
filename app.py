ddb53b8dc004bf733f1b64c0d421e2a5cbb5e348
luiz silva <48223061-l8758711@users.noreply.replit.com>
2026-01-30 14:11:26 +0000

    api

---
+++ app.py
@@ -0,0 +1,15 @@
+from controllers.bot_controller import bot_bp
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(bot_bp)
+
+@app.route('/')
+def index():
+    return jsonify({"API": "ONLINE"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ controllers/bot_controller.py
@@ -0,0 +1,25 @@
+from bot_worker import BotWorker
+from flask import Blueprint, jsonify, request
+
+bot_bp = Blueprint('bot', __name__, url_prefix="/api")
+
+b = BotWorker()
+
+@bot_bp.route('/question', methods=['POST'])
+def question():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "data inválida"}), 400
+    
+    pergunta = data.get("pergunta")
+    user_id = data.get("id")
+
+    if not pergunta:
+        return jsonify({"error": "Pergunta é obrigatória"}), 400
+
+    response = b.process_query(pergunta, user_id)
+
+    if not response:
+        return jsonify({"error": "Erro ao processar pergunta"}), 400
+
+    return jsonify({"resposta": response})
