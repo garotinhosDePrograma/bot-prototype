9c0cf7c9fe6ecc15c069d0adc217d449511e9644
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2026-02-02 22:30:43 -0300

    Update bot_controller.py

--- controllers/bot_controller.py
+++ controllers/bot_controller.py
@@ -3,7 +3,7 @@ Bot Controller - Rotas da API para o bot
 """
 
 from flask import Blueprint, jsonify, request
-from bot.workers.bot_worker import BotWorker
+from bot.bot_worker import BotWorker
 import logging
 
 logger = logging.getLogger(__name__)
